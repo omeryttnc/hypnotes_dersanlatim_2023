b35cf2af59ef9fa8be88b41b5ce286cd2e0d0e98
Nesibe Karan <nesibekrn@gmail.com>
2023-10-27 18:34:52 +0300

    week1 Grup2

---
+++ src/test/java/grup2/Task1.java
@@ -0,0 +1,41 @@
+package grup2;
+
+import org.junit.Test;
+
+import java.util.List;
+
+public class Task1 {
+
+    /*
+    List<String> names = List.of("Kesif","Plus","Selenium","Dersleri");
+print list into your console as STRUCTURAL PROGRAMING AND
+FUNCTIONAL PROGRAMING
+     */
+    List<String> names = List.of("Kesif","Plus","Selenium","Dersleri");
+
+    @Test
+   public void structural1(){
+        for (int i = 1; i < names.size()+1; i++) {
+            System.out.println("name "+i+" "+names.get(i-1));
+        }
+   }
+
+   @Test
+   public void structural2(){
+       for (String name:names) {
+           System.out.println(name);
+       }
+   }
+
+
+
+   //(x*y)=x+y+52
+    // (parameter)  -> functıon
+
+
+
+   @Test
+   public void functional(){
+        names.forEach(t-> System.out.println(t));
+   }
+}
---
+++ src/test/java/grup2/Task2.java
@@ -0,0 +1,37 @@
+package grup2;
+
+import org.junit.Test;
+
+import java.util.List;
+
+public class Task2 {
+
+/*
+List<String> names = List.of("Kesif","Plus","Selenium","Dersleri");
+print words only if they have equal or less than 5 letters
+ */
+List<String> names = List.of("Kesif","Plus","Selenium","Dersleri");
+    @Test
+    public void structural1(){
+        for (int i = 1; i < names.size()+1; i++) {
+            if (names.get(i-1).length()<=5){
+                System.out.println("name "+i+" "+names.get(i-1));
+            }
+        }
+    }
+
+    @Test
+    public void structural2(){
+        for (String each:names) {
+            if (each.length()<=5){
+                System.out.println(each);
+            }
+        }
+    }
+
+    @Test
+    public void functional1(){
+        names.stream().filter(t->t.length()<=5).forEach(System.out::println);
+    }
+
+}
