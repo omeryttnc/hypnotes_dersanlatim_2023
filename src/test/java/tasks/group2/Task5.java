f8f5fa9a06b87a96cbdeb362f43f5544facc48ce
nesib <nesibekrn@gmail.com>
2023-11-22 23:59:40 +0300

    task 5 added

--- src/test/java/tasks/group2/Task5.java
+++ src/test/java/tasks/group2/Task5.java
@@ -27,7 +27,7 @@ public class Task5 {
 
 
 
-// her hangi biri p iceriyor mu
+        // her hangi biri p iceriyor mu
         //Structural
         boolean flag2=false;
         for (String name:names) {
@@ -37,13 +37,13 @@ public class Task5 {
         }
         Assert.assertTrue(flag2);
 
-//Functional
+        //Functional
         boolean flagLambda2=names.stream().anyMatch(t->t.contains("p"));
         Assert.assertTrue(flagLambda2);
 
 
 
-// hic biri x icermiyor mu
+        // hic biri x icermiyor mu
         //Structural
         boolean flag3=true;
         for (String name:names) {
@@ -54,7 +54,7 @@ public class Task5 {
         }
         Assert.assertTrue(flag3);
 
-//        Functional
+        //Functional
         boolean flagLambda3=names.stream().noneMatch(t->t.contains("x"));
         Assert.assertTrue(flagLambda3);
 
@@ -68,7 +68,7 @@ public class Task5 {
         fruit.put("muz", 5);
         fruit.put("cilek", 2);
 
-// butun fiyatlar 0 dan buyuk
+        // butun fiyatlar 0 dan buyuk
         //Structural
         boolean flagMap1=true;
         for (int value:fruit.values()) {
@@ -78,13 +78,13 @@ public class Task5 {
         }
         Assert.assertTrue(flagMap1);
 
-//Functional
+        //Functional
         boolean flaqMapLambda1=fruit.values().stream().allMatch(t->t>0);
         Assert.assertTrue(flaqMapLambda1);
 
 
 
-// ama hicbiri 100 den buyuk degil
+        // ama hicbiri 100 den buyuk degil
         //Structural
         boolean flagMap2=true;
         for (int value:fruit.values()) {
@@ -94,13 +94,13 @@ public class Task5 {
         }
         Assert.assertTrue(flagMap2);
 
-//Functional
+        //Functional
         boolean flagMapLambda2=fruit.values().stream().noneMatch(t->t>100);
         Assert.assertTrue(flagMapLambda2);
 
 
 
-// ve herhangi biri 30 dan buyuk mu
+        // ve herhangi biri 30 dan buyuk mu
         //Structural
         boolean flagMap3=false;
         for (int value:fruit.values()) {
